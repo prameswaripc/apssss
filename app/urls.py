80558b22127714aa81f31e1a185ea632d5ad2ba2
prameswaripc <prameswaripc@student.ub.ac.id>
2022-10-03 09:38:24 +0700

    ahahah

--- app/urls.py
+++ app/urls.py
@@ -4,18 +4,18 @@ from.import views
 urlpatterns = [
     path('pasien', views.pasien, name='pasien'),
     path('createdatapasien', views.createdatapasien, name='createdatapasien'),
-    path('update/<str:id>', views.updatepasien, name='updatepasien'),
+    path('updatepasien/<str:id>', views.updatepasien, name='updatepasien'),
 
     path('dokter', views.dokter, name='dokter'),
     path('createdatadokter', views.createdatadokter, name='createdatadokter'),
-    path('update/<str:id>', views.updatedokter, name='updatedokter'),
-    path('delete/<str:id>', views.deletedokter, name='deletedokter'),
+    path('updatedokter/<str:id>', views.updatedokter, name='updatedokter'),
+    path('deletedokter/<str:id>', views.deletedokter, name='deletedokter'),
     
     path('pendaftaran', views.pendaftaran, name='pendaftaran'),
     path('createdatapendaftaran', views.createdatapendaftaran, name='createdatapendaftaran'),
     
     path('pelayanan', views.pelayanan, name='pelayanan'),
     path('createdatapelayanan', views.createdatapelayanan, name='createdatapelayanan'),
-    path('update/<str:id>', views.updatepelayanan, name='updatepelayanan'),
-    path('delete/<str:id>', views.deletepelayanan, name='deletepelayanan'),
+    path('updatepelayanan/<str:id>', views.updatepelayanan, name='updatepelayanan'),
+    path('deletepelayanan/<str:id>', views.deletepelayanan, name='deletepelayanan'),
 ]
